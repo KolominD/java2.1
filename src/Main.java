4971acab016811a6a0b3942113daafe7b562399f
KolominD <bogkolomind@gmail.com>
2021-09-14 13:02:56 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 5988;
+        int rublesForMile = 20;
+        int bonus = cost / rublesForMile;
+        System.out.println(bonus);
+
+    }
+}
